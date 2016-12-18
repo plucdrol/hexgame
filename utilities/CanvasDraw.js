a5d578f05a55984cf336ccabbe26e8f1822efce0
JasHT <jasminht@gmail.com>
2016-12-18 17:50:22 -0500

    Reorder CanvasDraw functions

--- utilities/CanvasDraw.js
+++ utilities/CanvasDraw.js
@@ -36,52 +36,6 @@ function CanvasDraw (canvas) {
         return this.canvas.height;
     }
 
-    //Draw a dot on the canvas at position 'point' with optional size and color
-    CanvasDraw.prototype.drawDot = function (point,size,color) {
-
-        //draw the dot as a very short line
-        var point1 = new Point(point.x-size/2,point.y);
-        var point2 = new Point(point.x+size/2,point.y);
-        this.drawLine(point1,point2,size,color)
-
-    };
-
-    //Draw a line on the canvas from p1 to p2 with optional width and color
-    CanvasDraw.prototype.drawLine = function(p1,p2,width,color) {
-
-        //express this function as a polygon
-        var points = [p1,p2];
-        this.drawPolygon(points,width,'transparent',color);
-
-/*
-        //defines the default size
-        if (typeof size === 'undefined') {
-            size = 1;
-        }
-        
-        //defines the default color
-        if (typeof color === 'undefined') {
-            color = 'black';
-        }
-
-        //create canvas line object
-        var line = this.canvas.getContext('2d');
-        
-
-        //define line path
-        line.beginPath();
-        line.moveTo(p1.x,p1.y);
-        line.lineTo(p2.x,p2.y);
-
-        //define line style
-        line.lineWidth = width;
-        line.lineCap = "butt";
-        line.strokeStyle = color;
-
-        //draw
-        line.stroke();*/
-    };
-
     //draw a canvas polygon touching each points, with optional line width, line color and polygon fill color
     CanvasDraw.prototype.drawPolygon = function(points,width,fill_color,line_color) {
         
@@ -94,12 +48,12 @@ function CanvasDraw (canvas) {
 
         //default line width
         if (typeof width === 'undefined') {
-            //width = 0;
+            width = 0;
         }
         
         //line style
         line.lineWidth = width;
-        line.lineCap="butt";
+        line.lineCap = "butt";
         line.strokeStyle = line_color;       
 
         //polygon outline
@@ -109,6 +63,7 @@ function CanvasDraw (canvas) {
             line.lineTo(points[i].x,points[i].y);
             //this.drawText(i,points[i],'black',24); //add the index of each corner
         }
+        //close polygons of at least 3 sides
         if (points.length > 2) {
             line.lineTo(points[0].x,points[0].y);
             line.closePath();
@@ -127,6 +82,25 @@ function CanvasDraw (canvas) {
             line.fill();
         }
         
+    };
+
+    //Draw a line on the canvas from p1 to p2 with optional width and color
+    CanvasDraw.prototype.drawLine = function(p1,p2,width,color) {
+
+        //express this function as a polygon
+        var points = [p1,p2];
+        this.drawPolygon(points,width,'transparent',color);
+
+    };
+
+    //Draw a dot on the canvas at position 'point' with optional size and color
+    CanvasDraw.prototype.drawDot = function (point,size,color) {
+
+        //draw the dot as a very short line
+        var point1 = new Point(point.x-size/2,point.y);
+        var point2 = new Point(point.x+size/2,point.y);
+        this.drawLine(point1,point2,size,color)
+
     };
 
     //draw text on the canvas at the specified position, with optional color and fontsize
