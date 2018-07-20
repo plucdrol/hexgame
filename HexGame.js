1fe9beaa54492c622ac28946d736dbbe1fb97641
JasHT <jasminht@gmail.com>
2018-07-20 18:11:42 -0400

    Extract the world-layer creation to a reusable function

--- HexGame.js
+++ HexGame.js
@@ -13,67 +13,64 @@ var canv_draw = new CanvasDraw(canvas);
 //Interface for receiving input from the page
 var canv_input = new CanvasInput(canvas);
 
-//Create a map generator
-var world_radius = 15;
-var hexmap_generator = new MapGenerator('perlin'); 
-var map = hexmap_generator.makeMap(world_radius);
-//and create a space map generator
-var space_radius = 25;
-var space_hexmap_generator = new MapGenerator('perlin'); 
-var space_map = space_hexmap_generator.makeMap(space_radius);
-//and create a galaxy map generator
-var galaxy_radius = 10;
-var galaxy_hexmap_generator = new MapGenerator('perlin'); 
-var galaxy_map = galaxy_hexmap_generator.makeMap(galaxy_radius);
+//This function creates a world and attaches all the necessary controllers
+//This doesn't seem like a very efficient way to do this
+function createWorldLayer(radius, scale, color_scheme) {
+  var world_radius = radius;
+  var hexmap_generator = new MapGenerator('perlin'); 
+  var map = hexmap_generator.makeMap(world_radius);
 
-//create a world
-var world = new World(world_radius);
-world.setMap(map);
-//create a space
-var space_world = new World(space_radius);
-space_world.setMap(space_map);
-//create a galaxy
-var galaxy_world = new World(galaxy_radius);
-galaxy_world.setMap(galaxy_map);
+  //create a world
+  var world = new World(world_radius);
+  world.setMap(map);
 
-//create a unit controller
-var unit_controller = new UnitController(map);
-  unit_controller.fillMap();
+  //create a unit controller
+  var unit_controller = new UnitController(map);
+  if (color_scheme != 'space') {
+    unit_controller.fillMap();
+  }
 
-//create a view for the galaxy map
-var galaxy_view = create_view(1/900);
-//create a default view for the space map
-var space_view = create_view(1/16);
-//create a view for the world map
-var view = create_view(3);
+  //create a view for the galaxy map
+  var view = create_view( scale );
 
+  //create a controller and renderer for the world
+  var world_interface = new WorldInterface(world, view, unit_controller);
+  if (color_scheme == 'space') {
+    var world_renderer = new WorldRenderer(canv_draw, view, world, unit_controller, 'space');  
+  } else {
+    var world_renderer = new WorldRenderer(canv_draw, view, world, unit_controller);
+  }
 
-//create a controller and renderer for the world
-var world_interface = new WorldInterface(world, view, unit_controller);
-var world_renderer = new WorldRenderer(canv_draw, view, world, unit_controller);
-//create a controller and renderer for the space
-var space_interface = new WorldInterface(space_world, space_view, false);
-var space_renderer = new WorldRenderer(canv_draw, space_view, space_world, unit_controller,'space');
-//create a controller and renderer for the space
-var galaxy_interface = new WorldInterface(galaxy_world, galaxy_view, false);
-var galaxy_renderer = new WorldRenderer(canv_draw, galaxy_view, galaxy_world, unit_controller,'space');
+  var layer_interface = {
+    world_interface: world_interface,
+    unit_controller: unit_controller,
+    world_renderer: world_renderer
+  }
+
+  return layer_interface;
+}
+var galaxy_layer_interface = createWorldLayer(40, 1/8064, 'space');
+var space_layer_interface = createWorldLayer(25, 1/128, 'space');
+var world_layer_interface = createWorldLayer(35, 1/2);
 
 //create a unit in the world
-unit_controller.createUnit(new Hex(0,0),'planet');
-unit_controller.createUnit(new Hex(1,0),'tree');
-unit_controller.createUnit(new Hex(25,-25),'water-player');
-unit_controller.createUnit(new Hex(25,0),'water-player');
-unit_controller.createUnit(new Hex(0,-25),'water-player');
-unit_controller.createUnit(new Hex(-25,-25),'water-player');
-unit_controller.createUnit(new Hex(-25,25),'water-player');
-unit_controller.createUnit(new Hex(-15,0),'water-player');
-unit_controller.createUnit(new Hex(1,0),'tree');
+world_layer_interface.unit_controller.createUnit(new Hex(0,0),'planet');
+world_layer_interface.unit_controller.createUnit(new Hex(1,0),'tree');
+world_layer_interface.unit_controller.createUnit(new Hex(25,-25),'water-player');
+world_layer_interface.unit_controller.createUnit(new Hex(25,0),'water-player');
+world_layer_interface.unit_controller.createUnit(new Hex(0,-25),'water-player');
+world_layer_interface.unit_controller.createUnit(new Hex(-25,-25),'water-player');
+world_layer_interface.unit_controller.createUnit(new Hex(-25,25),'water-player');
+world_layer_interface.unit_controller.createUnit(new Hex(-15,0),'water-player');
+world_layer_interface.unit_controller.createUnit(new Hex(1,0),'tree');
 
 
 canv_input.windowResize();
 ////////////////////////// DRAWING TO THE CANVAS //////////////////
 
-function drawScreen() {
+//world_interface, world_renderer, unit_controller, 
+
+function drawScreenBeforeInterfaces() {
 
   world_renderer.clear();
   //draw the galaxy
@@ -110,20 +107,41 @@ function drawScreen() {
   world_renderer.drawHex(world_interface.hex_hovered, hover_style );
 }
 
+function drawScreen() {
 
-///////////////////////////////////////// CHANGING THE WORLD //////////////////////////////////////////
-function newWorld() {
-  
-  var new_map =  hexmap_generator.makeMap(map_radius);
-  //update the world map
-  world.setMap(new_map);
+  world_layer_interface.world_renderer.clear();
+  //draw the galaxy
+  galaxy_layer_interface.world_renderer.drawWorld();
+  //draw the space
+  if (space_layer_interface.world_renderer.view.getZoom() > 0.06) {
+    space_layer_interface.world_renderer.drawWorld(); 
+  }
+  //draw the world
+  if (world_layer_interface.world_renderer.view.getZoom() > 0.06) {
+    world_layer_interface.world_renderer.drawWorld();    
+  }
 
-  //update the units 
-  unit_controller.newMap(new_map);
-  unit_controller.fillMap();
-  
-  drawScreen();
+
+  //draw range of selected unit
+  if (world_layer_interface.unit_controller.hex_selected instanceof Hex) {
+    var potentialUnit = world_layer_interface.unit_controller.units.getValue(world_layer_interface.unit_controller.hex_selected);
+    if (potentialUnit instanceof Unit && potentialUnit.hasComponent('range')) {
+      //world_renderer.drawPath(potentialUnit.components.range,world_interface.hex_hovered);
+      world_layer_interface.world_renderer.drawHexes(potentialUnit.getComponent('range'));
+    }
+
+    //draw selection hex
+    var select_style = new RenderStyle();
+    select_style.fill_color = "rgba(200,200,0,0.5)";
+    select_style.line_width = 2;
+    world_layer_interface.world_renderer.drawHex(world_layer_interface.unit_controller.hex_selected, select_style);
+  }
+
+  //draw hovered hex
+  var hover_style = new RenderStyle();
+  hover_style.fill_color = "rgba(200,200,200,0.4)";
+  hover_style.line_width = 0;
+  world_layer_interface.world_renderer.drawHex(world_layer_interface.world_interface.hex_hovered, hover_style );
 }
-
 ////////////////////////////////////////////////////// EVENT LISTENERS ////////////////////////////////////////
 canv_input.registerEvents();
