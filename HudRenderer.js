62f80f4dbe071d317ffe8cde411bc6a98e2cf4ec
JasHT <jasminht@gmail.com>
2018-07-28 09:21:08 -0400

    Simplify setup code for HudRenderer

--- HudRenderer.js
+++ HudRenderer.js
@@ -2,15 +2,13 @@ function HUDRenderer() {
 
   this.renderHUD = function(current_layer) {
 
-    console.log(current_layer);
     var world_interface = current_layer.world_interface;
-    var renderer = current_layer.world_renderer;
+    var world_renderer = current_layer.world_renderer;
     var controller = current_layer.unit_controller;
     var hex_selected = controller.hex_selected;
     var view = world_interface.view;
     var layout = world_interface.world.layout;
-    console.log(renderer);
-    var hex_renderer = new HexRenderer(renderer.hex_renderer.renderer.canvas_draw, view, layout);
+    var hex_renderer = world_renderer.hex_renderer;
 
     //selection draw
     if (hex_selected instanceof Hex) {
@@ -32,7 +30,7 @@ function HUDRenderer() {
     //draw hovered hex
     var hover_style = new RenderStyle();
     var hex_hovered = world_interface.hex_hovered;
-    console.log(hex_hovered);
+
     hover_style.fill_color = "rgba(200,200,200,0.4)";
     hover_style.line_width = 0;
     hex_renderer.drawHex( hex_hovered, hover_style );
