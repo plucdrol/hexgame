a0ad828daf1bac009acee9f768f919fe1e30ab79
JasHT <jasminht@gmail.com>
2018-08-06 10:52:39 -0400

    Whitespace

--- LayerManager.js
+++ LayerManager.js
@@ -71,7 +71,6 @@ function LayerManager() {
 	  layer.hex_center_offset = center_hex;
 
 	  //create a world object
-	  console.log('creating world');
 	  layer.world = new World(layer.offset, new Point(35/layer.scale, 35/layer.scale), radius, center_hex );// <-- point at which the sublayer affects this new layer
 
 	  //create a unit controller
