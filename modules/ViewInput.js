0c06fa0b4489826cb540017a83587c69bd3df298
JasminHT <JasminHT@users.noreply.github.com>
2023-08-13 11:09:22 -0400

    Rewrite View without THIS

--- modules/ViewInput.js
+++ modules/ViewInput.js
@@ -14,28 +14,23 @@ import Hex from './u/Hex.js'
 import {Point, HexLayout, HexMap} from './u/Hex.js'
 import Events from './u/Events.js'
 
-var render_update = false;
-
 export default function ViewInput(view) {
-  this.view = view;
 
-  this.world_drag = new Point(0,0);
-  this.screen_drag = new Point(0,0);
+  var world_drag = new Point(0,0);
+  var screen_drag = new Point(0,0);
 
-  this.zoom_amount = 1;
+  var zoom_amount = 1;
 
 
-  //Event handling
-  var self = this;
-
+  //Event listening
   Events.on('canvas_zoom', function(e){
-    self.zoomViewEvent(e.detail.amount);
+    zoomViewEvent(e.detail.amount);
   } );
   Events.on('canvas_drag', function(e){
-    self.dragEvent(e.detail.mousepos,e.detail.mouseposprevious);
+    dragEvent(e.detail.mousepos,e.detail.mouseposprevious);
   } );
   Events.on('canvas_resize', function(e){
-    self.resizeEvent(e.detail.width, e.detail.height);
+    resizeEvent(e.detail.width, e.detail.height);
   } );
   document.addEventListener('keydown', logKey);
 
@@ -46,10 +41,10 @@ export default function ViewInput(view) {
   function logKey(event) {
     console.log(event.keyCode);
     if (event.keyCode === 189 || event.keyCode === 173) { // minus
-        self.zoomViewEvent(1.2);
+        zoomViewEvent(1.2);
     }
     if (event.keyCode === 187 || event.keyCode === 61) { // plus
-        self.zoomViewEvent(0.8);
+        zoomViewEvent(0.8);
     }
 
     return false;
@@ -57,19 +52,19 @@ export default function ViewInput(view) {
 
 
   //React to either mouse scrolling or finger pinching
-  this.zoomViewEvent = function(zoom) {
+  function zoomViewEvent(zoom) {
 
-    this.zoom_amount *= zoom; //1.2 or 0.8
+    zoom_amount *= zoom; //1.2 or 0.8
 
-    if (1-this.zoom_amount <= -0.2) { // 1 - 1.1 = -=0.1     1 - 1.3 = -0.3
-      this.view.zoom(this.zoom_amount);
-      //console.log(this.zoom_amount);
-      this.zoom_amount = 1;
+    if (1-zoom_amount <= -0.2) { // 1 - 1.1 = -=0.1     1 - 1.3 = -0.3
+      view.zoom(zoom_amount);
+      //console.log(zoom_amount);
+      zoom_amount = 1;
     }
-    if (1-this.zoom_amount <= 0.2) { // 1 - 0.7 = 0.3,  1-0.9 = 0.1
-      this.view.zoom(this.zoom_amount);
-      //console.log(this.zoom_amount);
-      this.zoom_amount = 1;
+    if (1-zoom_amount <= 0.2) { // 1 - 0.7 = 0.3,  1-0.9 = 0.1
+      view.zoom(zoom_amount);
+      //console.log(zoom_amount);
+      zoom_amount = 1;
     }
   }
   
@@ -77,15 +72,15 @@ export default function ViewInput(view) {
 
 
   //React to dragging across the screen with finger or mouse
-  this.dragEvent = function(mouse, previous_mouse) {
+  function dragEvent(mouse, previous_mouse) {
 
     //get the movement the mouse has moved since last tick
-    var x_move = this.view.screenToWorld1D(previous_mouse.x-mouse.x);
-    var y_move = this.view.screenToWorld1D(previous_mouse.y-mouse.y);
+    var x_move = view.screenToWorld1D(previous_mouse.x-mouse.x);
+    var y_move = view.screenToWorld1D(previous_mouse.y-mouse.y);
     var drag_move = new Point(x_move, y_move);
 
     //shift the view by that movement
-    this.view.shiftPosition(drag_move);
+    view.shiftPosition(drag_move);
 
     //shift the image in the temporary canvas
     var temp_context = canvas.getContext('2d');
@@ -93,18 +88,18 @@ export default function ViewInput(view) {
   }
 
 
-  this.actuallyDrag = function() {
+  function actuallyDrag() {
 
-    this.view.shiftPosition(this.world_drag);
+    view.shiftPosition(world_drag);
 
-    this.world_drag = new Point(0,0);
-    this.screen_drag = new Point(0,0);
+    world_drag = new Point(0,0);
+    screen_drag = new Point(0,0);
   }
 
 
   //React to the window being resized
-  this.resizeEvent = function(width, height) {
-    this.view.resizeOutput(width, height);
+  function resizeEvent(width, height) {
+    view.resizeOutput(width, height);
   }
 
 }
